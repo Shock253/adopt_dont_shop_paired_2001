7b713a44d30fe5844e484c7c37a8ff13498cecce
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-25 14:59:37 -0600

    Update shelter_review controller

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -4,6 +4,21 @@ class ShelterReviewsController < ApplicationController
     @review = @shelter.shelter_reviews.find(params[:review_id])
   end
 
+  def new
+   @shelter = Shelter.find(params[:id])
+  end
+
+  def create
+    shelter = Shelter.find(params[:id])
+    review = shelter.shelter_reviews.create(shelter_review_params)
+    if review.save
+        redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:notice] = "Could not create shelter: Please make sure to enter title, rating, and content"
+      redirect_to "/shelters/#{shelter.id}/reviews/new"
+    end
+  end
+
   def update
     shelter = Shelter.find(params[:id])
     review = ShelterReview.find(params[:review_id])
