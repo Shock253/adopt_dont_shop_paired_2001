ad4d60f0c32bdb657288b4d8ca5ffc895dc3f7a0
Ethan Hocking <eshocking253@gmail.com>
2020-03-25 11:29:45 -0600

    Add route for new shelter review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
@@ -8,10 +9,14 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete 'shelters/:id', to: 'shelters#destroy'
+
   get '/shelters/:id/pets', to: 'shelter_pets#index'
   get '/shelters/:id/pets/new', to: 'pets#new'
+
   get '/shelters/:id/reviews/:review_id/edit', to: 'shelter_reviews#edit'
   patch '/shelters/:id/reviews/:review_id', to: 'shelter_reviews#update'
+  get '/shelters/:id/reviews/new', to: 'shelter_reviews#new'
+
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
@@ -20,4 +25,5 @@ Rails.application.routes.draw do
   patch '/pets/:id', to: 'pets#update'
   post '/shelters/:id/pets', to: 'pets#create'
   delete '/pets/:id', to: 'pets#destroy'
+
 end
