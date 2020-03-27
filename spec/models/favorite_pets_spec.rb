cf19023e8d9502cfbe937e715691f86ffebce519
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-27 12:44:01 -0600

    Add remove_favorites method test to favorite_pets poro

--- spec/models/favorite_pets_spec.rb
+++ spec/models/favorite_pets_spec.rb
@@ -66,4 +66,38 @@ RSpec.describe FavoritePets, type: :model do
         expect(favorite_pets.total_count).to eq(3)
     end
   end
+
+  describe "#remove_pet" do
+    it "can remove pets" do
+      shelter_1 = Shelter.create!(name: "Denver Animal Shelter",
+                                address: "500 Invisible St.",
+                                city: "Denver",
+                                state: "Colorado",
+                                zip: "80201")
+
+      pet_1 = Pet.create(image: 'app/assets/images/border_collie.jpg',
+                      name: 'Rover',
+                      age: 3,
+                      sex: "Male",
+                      shelter: shelter_1,
+                      description: "He's a biter.",
+                      status: "Pending")
+
+      pet_2 = Pet.create(image: 'app/assets/images/border_collie.jpg',
+                      name: 'Sam',
+                      age: 3,
+                      sex: "Male",
+                      shelter: shelter_1,
+                      description: "He's a biter.",
+                      status: "Adoptable")
+
+      favorite_pets = FavoritePets.new([
+          pet_1.id,
+          pet_2.id
+        ])
+
+        favorite_pets.remove_pet(pet_1.id)
+        expect(favorite_pets.total_count).to eq(1)
+    end
+  end
 end
