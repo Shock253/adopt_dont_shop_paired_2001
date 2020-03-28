64291ca435af472adfe0f11e56eb83de9b1984c1
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-28 12:07:36 -0600

    Create application model, create application pets model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,8 @@ class Pet < ApplicationRecord
   after_initialize :set_defaults, unless: :persisted?
   belongs_to :shelter
   validates_presence_of :image, :name, :age, :sex, :description, :status
+  has_many :application_pets
+  has_many :applications, through: :application_pets
 
   def set_defaults
     self.status ||= 'Adoptable'
