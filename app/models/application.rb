9b1b0781e6813222357f3f8ff736eabb9d499ef0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-30 16:15:37 -0600

    Add dependent destroy clauses for pets to be deleted

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :phone_number, :description
-  has_many :application_pets
+  has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
 end
