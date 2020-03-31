eedc8d8c1feaeacb6c7a9a1b4d438c76f8dd86d0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-31 11:53:00 -0600

    Add flash messages to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -20,8 +20,12 @@ class PetsController < ApplicationController
   def create
     shelter = Shelter.find(params[:id])
     pet = shelter.pets.create(pet_params)
-    pet.save
-    redirect_to "/shelters/#{shelter.id}/pets"
+    if pet.save
+      redirect_to "/shelters/#{shelter.id}/pets"
+    else
+      flash[:notice] = "Required fields are missing, please enter all information required."
+      redirect_to "/shelters/#{shelter.id}/pets/new"
+    end
   end
 
   def edit
@@ -31,8 +35,12 @@ class PetsController < ApplicationController
   def update
     pet = Pet.find(params[:id])
     pet.update(pet_params)
-    pet.save
-    redirect_to "/pets/#{pet.id}"
+    if pet.save
+      redirect_to "/pets/#{pet.id}"
+    else
+      flash[:notice] = "Required fields are missing, please enter all information required."
+      redirect_to "/pets/#{pet.id}/edit"
+    end
   end
 
   def destroy
