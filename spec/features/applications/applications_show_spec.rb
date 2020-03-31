d2fee1cff19706ec2f918d058e3e2f6514ab5cc4
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-30 22:54:48 -0600

    Add test for pets on application show pages to only be approved for adoption once

--- spec/features/applications/applications_show_spec.rb
+++ spec/features/applications/applications_show_spec.rb
@@ -100,4 +100,33 @@ RSpec.describe "Application show page" do
     expect(page).to have_content("Pending")
     expect(page).to have_content("John Wick")
   end
+
+  it "can not approve the same pet for multiple applications" do
+    application_2 = Application.create(
+                    name: "Luke Skywalker",
+                    address: "450 S. Cherry St.",
+                    city: "Aldoran",
+                    state: "CO",
+                    zip: "19999",
+                    phone_number: "8007891234",
+                    description: "I have a big yard")
+
+    ApplicationPet.create(application: application_2, pet: @pet_1)
+
+    visit "/applications/#{@application.id}"
+    within "#pet-#{@pet_1.id}" do
+      click_link "Approve #{@pet_1.name} for Adoption"
+    end
+
+    visit "/applications/#{application_2.id}"
+
+    within "#pet-#{@pet_1.id}" do
+      expect(page).to_not have_link("Approve #{@pet_1.name}")
+      expect(page).to have_content("#{@pet_1.name} is currently pending adoption.")
+    end
+
+    click_button "Approve Pets"
+    expect(page).to have_current_path("/applications/#{application_2.id}")
+    expect(page).to have_content("There are currently no pets selected for approval to adopt!")
+  end
 end
