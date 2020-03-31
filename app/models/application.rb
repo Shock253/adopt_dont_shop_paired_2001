eaadc7fd3d14e2ffe12b5c74dc7bfc21952804b2
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-31 00:23:29 -0600

    Add pending_approval? query method to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,8 @@ class Application < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :phone_number, :description
   has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
+
+  def pending_approval?(pet_id_param)
+    application_pets.where(pet_id: pet_id_param).first.status == "Pending Approval"
+  end
 end
