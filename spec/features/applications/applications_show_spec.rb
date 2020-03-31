31f98b6b822fdf9daac88b26483bca447cbaa0fe
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-31 00:24:55 -0600

    Add edge case test to applications show spec

--- spec/features/applications/applications_show_spec.rb
+++ spec/features/applications/applications_show_spec.rb
@@ -129,9 +129,25 @@ RSpec.describe "Application show page" do
   end
 
   it "can revoke applications for a pet that has been approved" do
+    application_2 = Application.create(
+                    name: "Luke Skywalker",
+                    address: "450 S. Cherry St.",
+                    city: "Aldoran",
+                    state: "CO",
+                    zip: "19999",
+                    phone_number: "8007891234",
+                    description: "I have a big yard")
+
+    ApplicationPet.create(application: application_2, pet: @pet_1)
+
     visit "/applications/#{@application.id}"
     within "#pet-#{@pet_1.id}" do
       click_link "Approve #{@pet_1.name} for Adoption"
+    end
+
+    visit "/applications/#{application_2.id}"
+    within "#pet-#{@pet_1.id}" do
+      expect(page).to_not have_link("Revoke #{@pet_1.name}'s Application")
     end
 
     visit "/applications/#{@application.id}"
